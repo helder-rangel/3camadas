375fdc02b42b641d0fc6e477371040232ecd55e3
Helder Jerônimo Leite Rangel <helder.rangel@academico.ifpb.edu.br>
2018-06-23 11:39:12 -0300

    Add files via upload

--- fachada/Fachada.java
+++ fachada/Fachada.java
@@ -1,5 +1,8 @@
 package fachada;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
 import modelo.Conta;
 import modelo.Garcom;
 import modelo.Mesa;
@@ -8,14 +11,78 @@ import repositorio.Restaurante;
 
 public class Fachada {
 
-	public static void addGarcom(Garcom g) {
-		Garcom.garcons.add(g);
+
+	// coloquei métodos da fachada, seguindo documentação
+	// deve buscar por algo que for digitado
+	public static ArrayList<Produto> listarProdutos(Produto nome){
+		return ArrayList<Produto>;
+		}
+	//ao digitar enter, deve listar todos os produtos
+	public static ArrayList<Produto> listarProdutos(ActionEvent e){
+		return ArrayList<Produto>;
+		}
+
+	public static ArrayList<Garcom> listarGarcons(){
+		return ArrayList<Garcom>;
+		}
+
+	public static ArrayList<Mesa> listarMesass(){
+		return ArrayList<Mesa>;
+		}
+
+	public static ArrayList<Conta> listarContas(){
+		return ArrayList<Conta>;
+		}
+
+	public static void criarMesas(int n){
+		
+		
 	}
+
+	public static Produto cadastrarProduto(String nome, double preco) {
+		return Produto;
+	}
+
+	public static Garcom cadastrarGarcom(String apelido, int[] mesainicial, int[] mesafinal){
+		return Garcom;
+	}
+
+	public static Conta criarConta(int idmesa){
+		return idmesa;
+	}
+
+	public static Conta consultarConta(int idmesa){
+		return Conta;
+	}
+
+	public static Produto solicitarProduto(int idmesa, String nomeproduto){
+		return Produto;
+	}
+
+	public static void cancelarConta(int idmesa){
 	
+	}
+
+	public static void transferirConta(int idmesaorigem, int idmesadestino){
+	
+	}
+
+	public static void fecharConta(int idmesa){
+	
+	}
+
+	public static double calcularGorjeta(String apelido){
+		return;
+	}
+
+	public static void addGarcom(Garcom g) {
+		
+	}
+
 	public static void removeGarcom(Garcom g){
 		garcons.remove(g);
 	}	
-	
+
 	public static Garcom buscarGarcom(String apelido) {
 		for(Garcom c:garcons) {
 			if(c.getApelido().equals(apelido)) {
@@ -24,15 +91,15 @@ public class Fachada {
 		}
 		return null;
 	}
-	
+
 	public static void addConta(Conta c) {
 		contas.add(c);
 	}
-	
+
 	public static void removeConta(Conta c) {
 		contas.remove(c);
 	}
-	
+
 	public static Conta buscarConta(int id) {
 		for(Conta c:contas) {
 			if(c.getNumero()==id) {
@@ -41,15 +108,15 @@ public class Fachada {
 		}
 		return null;
 	}
-	
+
 	public static void addMesa(Mesa m) {
 		mesas.add(m);
 	}
-	
+
 	public static void removeMesa(Mesa m) {
 		mesas.remove(m);
 	}
-	
+
 	public static Mesa buscarMesa(int id) {
 		for(Mesa m:mesas) {
 			if(m.getId()==id) {
@@ -58,15 +125,15 @@ public class Fachada {
 		}
 		return null;
 	}
-	
+
 	public static Produto addProduto(String no, double pre){
 		return addProduto(no,pre);
 	}
-	
+
 	public static void removeProduto(Produto p) {
 		produtos.remove(p);
 	}
-	
+
 	public static Produto buscarProduto(String nome) {
 		for(Produto p:produtos) {
 			if(p.getNome().equals(nome)) {
